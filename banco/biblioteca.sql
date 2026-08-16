77eaff3124648d271f8afc5ca49a7ad8f54d6012
Iury Nicolau <iurynicolau8@gmail.com>
2026-08-15 21:03:45 -0300

    biblioteca.sql and consultas.sql changings

--- banco/biblioteca.sql
+++ banco/biblioteca.sql
@@ -1,150 +1,84 @@
--- ============== TABELAS ================
--- tabela livros
-create table livros(
- id_livro integer auto_increment primary key,
- id_autor int not null,
- titulo text not null,
- ano integer,
-
- foreign key (id_autor) references autores(id_autor)
- );
- 
--- tabela alunos
-create table alunos(
- id_aluno int auto_increment primary key,
- nome text not null,
- idade int not null,
- curso text not null
- );
- 
--- tabela autores
 create table autores(
 id_autor int auto_increment primary key,
 nome text not null,
-nacionalidade text
+nacionalidade text not null
 );
 
--- tabela emprestimos
+create table livros(
+id_livro int auto_increment primary key,
+titulo text not null,
+id_autor int not null,
+
+foreign key (id_autor) references autores(id_autor)
+);
+
+create table usuarios(
+id_usuario int auto_increment primary key,
+nome text not null,
+idade text not null,
+curso text not null
+);
+
 create table emprestimos(
 id_emprestimo int auto_increment primary key,
-id_aluno int not null,
+id_usuario int not null,
 id_livro int not null,
 data_emprestimo text not null,
-data_devolucao text,
+data_devolucao text not null,
 
-foreign key (id_aluno) references alunos(id_aluno),
-foreign key (id_livro) references livros(id_livro)
+foreign key (id_livro) references livros(id_livro),
+foreign key (id_usuario) references usuarios(id_usuario)
 );
 
--- ============== INSERTS ================
--- adiciona a livros 
-INSERT INTO livros(titulo,id_autor,ano)
- values('Harry Potter', 4,1997);
- 
-INSERT INTO livros(titulo,id_autor,ano)
- values('Dom Casmurro', 3,1899);
- 
-INSERT INTO livros(titulo,id_autor,ano)
- values('1984', 2,1949);
- 
- INSERT INTO livros(titulo,id_autor,ano)
- values('O Hobbit', 1,1937);
+insert into autores(nome,nacionalidade) values ("Machado de Assis","Brasileira");
+insert into autores(nome,nacionalidade) values ("Agatha Christie","Britânica");
+insert into autores(nome,nacionalidade) values ("Gabriel García Márquez","Colombiana");
+insert into autores(nome,nacionalidade) values ("Stephen King","Americana");
+insert into autores(nome,nacionalidade) values ("Clarice Lispector","Brasileira");
 
--- isere a emprestimos
-insert into emprestimos(id_aluno,id_livro,data_emprestimo) values
-(1,1,'22/06'),
-(2,2,'12/09'),
-(3,4,'25/01'),
-(4,1,'11/04'),
-(1,4,'01/03');
+insert into livros(titulo,id_autor) values ("Dom Casmurro",'1');
+insert into livros(titulo,id_autor) values ("Memórias Póstumas de Brás Cubas",'1');
+insert into livros(titulo,id_autor) values ("Quincas Borba",'1');
+insert into livros(titulo,id_autor) values ("O Assassinato no Expresso do Oriente",'2');
+insert into livros(titulo,id_autor) values ("Morte no Nilo",'2');
+insert into livros(titulo,id_autor) values ("E Não Sobrou Nenhum",'2');
+insert into livros(titulo,id_autor) values ("Cem Anos de Solidão",'3');
+insert into livros(titulo,id_autor) values ("O Amor nos Tempos do Cólera",'3');
+insert into livros(titulo,id_autor) values ("Crônica de uma Morte Anunciada",'3');
+insert into livros(titulo,id_autor) values ("O Iluminado",'4');
+insert into livros(titulo,id_autor) values ("It: A Coisa",'4');
+insert into livros(titulo,id_autor) values ("Misery: Louca Obsessão",'4');
+insert into livros(titulo,id_autor) values ("A Hora da Estrela",'5');
+insert into livros(titulo,id_autor) values ("Perto do Coração Selvagem",'5');
+insert into livros(titulo,id_autor) values ("Laços de Família",'5');
 
--- insere a autores
-insert into autores(nome,nacionalidade) values
-('J.R.R Tolkien','Britânico'),
-('George Orwell','Britânico'),
-('Machado de Assis','Brasileiro'),
-('J.K. Rowling','Britânica');
+insert into usuarios(nome,idade,curso) values ("Lucas Silva",'20',"ADS");
+insert into usuarios(nome,idade,curso) values ("Mariana Santos",'22',"SI");
+insert into usuarios(nome,idade,curso) values ("Carlos Oliveira",'25',"Gestao");
+insert into usuarios(nome,idade,curso) values ("Ana Costa",'19',"ADS");
+insert into usuarios(nome,idade,curso) values ("Beatriz Souza",'21',"SI");
+insert into usuarios(nome,idade,curso) values ("Rodrigo Lima",'23',"Gestao");
+insert into usuarios(nome,idade,curso) values ("Gabriel Almeida",'26',"ADS");
+insert into usuarios(nome,idade,curso) values ("Amanda Ribeiro",'20',"SI");
+insert into usuarios(nome,idade,curso) values ("Juliana Pereira",'24',"Gestao");
+insert into usuarios(nome,idade,curso) values ("Felipe Rodrigues",'18',"ADS");
 
--- ============== EXIBICOES ================
--- exibe nome/titulo/dataEmprestimo
-select
-alunos.nome,
-livros.titulo,
-emprestimos.data_emprestimo
-from emprestimos
-inner join alunos
-on emprestimos.id_aluno = alunos.id_aluno
-inner join livros
-on emprestimos.id_livro = livros.id_livro;
-
--- exibe nome/titulo/dataEmprestimo/dataDevolucao
-select
-alunos.nome,
-livros.titulo,
-emprestimos.data_emprestimo,
-emprestimos.data_devolucao
-from emprestimos
-inner join alunos
-on emprestimos.id_aluno = alunos.id_aluno
-inner join livros
-on emprestimos.id_livro = livros.id_livro
-order by data_emprestimo desc;
-
--- exibe aluno/titulo/autor/dataEmprestimo
-select
-alunos.nome as aluno,
-livros.titulo as livro,
-autores.nome as autor,
-emprestimos.data_emprestimo as dataEmprestimo
-from emprestimos
-inner join alunos
-on emprestimos.id_aluno = alunos.id_aluno
-inner join livros
-on emprestimos.id_livro = livros.id_livro
-inner join autores
-on livros.id_autor = autores.id_autor;
-
--- exibe livros/autores
-select
-livros.titulo as Titulo,
-autores.nome as Autor
-from livros
-inner join autores
-on livros.id_autor = autores.id_autor;
-
--- exibe quantidade de elementos em livros
-select count(*) from livros;
-
--- exibe a quantidade de livros por autores
-select 
-autores.nome as autor,
-count(*) as quantidade_livros
-from autores
-group by autores.nome;
-
--- mesma coisa que a anterior com filtro
-SELECT
-    autores.nome AS autor,
-    COUNT(livros.id_livro) AS quantidade_livros
-FROM autores
-INNER JOIN livros
-    ON autores.id_autor = livros.id_autor
-GROUP BY autores.id_autor
-HAVING COUNT(livros.id_livro) > 1;
-
--- exibe o maior valor de ano
-select max(ano) from livros;
-
--- exibe o valor min de ano
-select min(ano) from livros;
-
--- exibe a media das idades
-select avg(idade) from alunos;
-
--- exibe livros/autores
-select
-livros.titulo as Titulo,
-autores.nome as Autor
-from livros
-inner join autores
-on livros.id_autor = autores.id_autor;
+insert into emprestimos(id_usuario,id_livro,data_emprestimo,data_devolucao) values ("1","3","2026-02-01","2026-02-15");
+insert into emprestimos(id_usuario,id_livro,data_emprestimo,data_devolucao) values ("2","1","2026-02-01","2026-02-15");
+insert into emprestimos(id_usuario,id_livro,data_emprestimo,data_devolucao) values ("3","7","2026-02-02","2026-02-16");
+insert into emprestimos(id_usuario,id_livro,data_emprestimo,data_devolucao) values ("4","10","2026-02-03","2026-02-17");
+insert into emprestimos(id_usuario,id_livro,data_emprestimo,data_devolucao) values ("5","13","2026-02-04","2026-02-18");
+insert into emprestimos(id_usuario,id_livro,data_emprestimo,data_devolucao) values ("6","4","2026-02-05","2026-02-19");
+insert into emprestimos(id_usuario,id_livro,data_emprestimo,data_devolucao) values ("7","14","2026-02-06","2026-02-20");
+insert into emprestimos(id_usuario,id_livro,data_emprestimo,data_devolucao) values ("8","2","2026-02-07","2026-02-21");
+insert into emprestimos(id_usuario,id_livro,data_emprestimo,data_devolucao) values ("9","11","2026-02-08","2026-02-22");
+insert into emprestimos(id_usuario,id_livro,data_emprestimo,data_devolucao) values ("10","8","2026-02-09","2026-02-23");
+insert into emprestimos(id_usuario,id_livro,data_emprestimo,data_devolucao) values ("1","5","2026-02-10","2026-02-24");
+insert into emprestimos(id_usuario,id_livro,data_emprestimo,data_devolucao) values ("3","12","2026-02-11","2026-02-25");
+insert into emprestimos(id_usuario,id_livro,data_emprestimo,data_devolucao) values ("5","6","2026-02-12","2026-02-26");
+insert into emprestimos(id_usuario,id_livro,data_emprestimo,data_devolucao) values ("2","15","2026-02-13","2026-02-27");
+insert into emprestimos(id_usuario,id_livro,data_emprestimo,data_devolucao) values ("4","9","2026-02-14","2026-02-28");
+insert into emprestimos(id_usuario,id_livro,data_emprestimo,data_devolucao) values ("7","3","2026-02-15","2026-03-01");
+insert into emprestimos(id_usuario,id_livro,data_emprestimo,data_devolucao) values ("6","1","2026-02-16","2026-03-02");
+insert into emprestimos(id_usuario,id_livro,data_emprestimo,data_devolucao) values ("8","10","2026-02-18","2026-03-04");
+insert into emprestimos(id_usuario,id_livro,data_emprestimo,data_devolucao) values ("10","13","2026-02-19","2026-03-05");
